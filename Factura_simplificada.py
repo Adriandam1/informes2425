15c7250b5996f66e21bd047f3e9f32c84c7001a5
Adriamdam1 <aabeijoncarbajo@danielcastelao.org>
2025-02-14 14:04:10 +0100

    update 14-2-25 Manuel empezando factura2

--- Factura_simplificada.py
+++ Factura_simplificada.py
@@ -12,6 +12,57 @@ from reportlab.lib import colors
 from reportlab.lib.enums import TA_RIGHT
 from reportlab.lib.styles import ParagraphStyle
 
+# -------------------------------------------------
+#PARA BASE DE DATOS MANUEL ESTA HACIENDO EL EJEMPLO FACTURA2
+import sqlite3 as dbapi
+
+marxeSup = A4[1] - 2*cm
+marxeInf = 1.5*cm
+marxeEsq = 3*cm
+marxeDer = A4[0] - 1.5*cm
+anchoPaxina = marxeDer - marxeEsq
+
+bbdd = dbapi.connect("bbdd.dat")
+cursor = bbdd.cursor()
+cursor.execute("""Select id_factura, fecha_emision, id_empresa, numero_factura from Factura""")
+
+logo = []
+facturas = []
+for factura in cursor.fetchall():
+    facturas.append (factura)
+
+# Estamos con la esquina superior derecha
+for i, factura in enumerate (facturas):
+    c = Canvas('facturaNum' + str(i)+'.pdf', pagesize=A4) # creo un canva por cada factura, si quisexemes 2 facturas por paxina usariamos A2
+    logo.append(Image ("tick.png", 100, 55))
+    frmLogo =   Frame (marxeDer-100, marxeSup-55, 100, 70, showBoundary=0)
+    frmLogo.addFromList(logo, c) # lle digo que utilize a lista sobre o canvas
+    tx = c.beginText(marxeDer-105, marxeSup-60) # creo un obxeto texto que o situei debaixo do logo
+    tx.setFont("Helvetica", 14)
+    tx.textLine("Electropachachos")
+    c.drawText(tx) # estamos pintando  o texto no canvas
+
+    frmDatosFactura = Frame (marxeDer -100, marxeSup -120, 90, 50, showBoundary=0)
+    tblDatosFact = Table (data= (('Data de emisión: ', factura[1]), ('Número factura: '. factura[3])))
+    frmDatosFactura.addFromList(list().append(tblDatosFact), c) # creo unha lista e lle engado un elemento e lle paso o canvas para que o rexistre ahi
+
+
+    cursor.execute("""Select nome, direccion, cidade, pais, email, cid_nif, telefono From Empresa Where id_empresa = ?""", (factura[2],))
+    frmDatosCliente =
+    tblDatosCliente
+
+
+
+
+
+
+
+
+
+# -------------------------------------------------
+
+
+
 follaEstilo = getSampleStyleSheet()
 documento = []
 estilo = follaEstilo['BodyText']
@@ -122,14 +173,22 @@ documento.append(tabla)
 
 # ----------------------------------------
 
-
+# COLUMNA VERDE -----------------------
+# Función para dibujar la columna verde a la izquierda del documento
+# esto nos fuerza a ponerle un margen izquierdo al fram de +1*cm para que no se superpongan
+# tambien debemos añadirlo con onPage=draw_background en el addPageTemplates
+def draw_background(canvas, doc):
+    canvas.saveState()
+    canvas.setFillColor(colors.mediumseagreen)
+    canvas.rect(0, 0, 2*cm, A4[1], fill=1)
+    canvas.restoreState()
 
 # Crear el documento PDF
 doc = BaseDocTemplate("Factura_simplificada.pdf", pagesize=A4)
 # Definir el marco de la página
-frame = Frame(doc.leftMargin, doc.bottomMargin, doc.width, doc.height, id='normal')
+frame = Frame(doc.leftMargin +1*cm, doc.bottomMargin, doc.width, doc.height, id='normal')
 # Añadir la plantilla de página
-template = PageTemplate(id='test', frames=frame)
+template = PageTemplate(id='test', frames=frame, onPage=draw_background)
 doc.addPageTemplates([template])
 # Construir el documento
 doc.build(documento)
